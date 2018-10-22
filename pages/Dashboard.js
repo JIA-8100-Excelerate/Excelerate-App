19d827afb3d1244920ef2cc87cf0dc75f8aaaea4
ksun86 <ksun68@gatech.edu>
2018-10-22 17:20:47 -0400

    added Dashboard and Goal_Summary pages

---
+++ pages/Dashboard.js
@@ -0,0 +1,121 @@
+import React, {Component} from 'react';
+import {Button, Text, View, StyleSheet, Image, TextInput, TouchableOpacity} from 'react-native';
+import {StackActions, NavigationActions} from 'react-navigation';
+
+class Dashboard extends Component {
+  constructor(props){
+     super(props);
+     this.state = {       
+        summary: '',
+     }
+  }
+
+  render() {
+    const { navigate } = this.props.navigation;
+    const firstName = this.props.navigation.getParam('name', 'GuitarBob99');
+    const actions = this.props.navigation.getParam('actions', 'nothing');
+    const goalType = this.props.navigation.getParam('goalType', 'Rip');
+    const newGoal = this.props.navigation.getParam('newGoal', false);
+    this.state.count = 1;
+    this.state.summary = '';
+    console.log(goalType);
+    console.log(newGoal);
+    console.log(actions);
+    for (let i = 0; i < 5; i++) {
+      if (actions[i] != '') {
+        this.state.summary+= "\n" + this.state.count.toString() + '. ' + actions[i];
+        this.state.count++;
+      } 
+    }
+    return (
+      <View style={styles.container}>
+        <Text style={styles.headerText}>
+          Welcome, {firstName}!
+        </Text> 
+        {newGoal &&
+          <Text style={styles.actionText}>  To accomplish your new {goalType} goal, you will: {this.state.summary} </Text> 
+        }       
+        <View style={styles.button}>
+          <Button 
+            title= "Set a New Goal"
+            color= "#ffffff"       
+            onPress={() => {
+                navigate('Set_Goal', { name: firstName});
+          }}
+            />
+        </View>
+        <View style={styles.button}>
+          <Button
+            title= "View My Goals"
+            color= "#ffffff"
+            onPress={() => {
+                navigate('Set_Goal');
+          }}
+            />
+          </View>
+        <View style={styles.button}>
+          <Button
+            title= "View My Accomplishments"
+            color= "#ffffff"
+            onPress={() => {
+                navigate('Set_Goal');
+            }}
+          />
+        </View>  
+        <View style={styles.button}>  
+          <Button
+                title= "Logout"
+                color='#ffffff'
+                fontSize = '30'
+                onPress={() => {
+                  navigate('Login');
+            }}
+          />     
+          </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#03a9f4',
+    flex: 1,
+    alignItems:'center',
+    justifyContent:'center'
+  },
+  headerText: {
+    color: 'white',
+    fontSize: 40,
+    textAlign: 'center',
+    marginBottom: 50
+  },
+  imageStyle: {
+    backgroundColor: '#03a9f4',
+    width: 207,
+    height: 210,
+    margin: 15,
+    marginBottom: 50,
+  }, 
+  actionText: {
+    fontSize: 30, 
+    fontWeight: 'bold',    
+    color: '#ffffff',
+    alignItems:'center', 
+    paddingHorizontal: 30,
+  },
+  button: {
+    width: 300,
+    height: 40,
+    backgroundColor: '#01579b',
+    borderRadius: 20,
+    borderWidth: 2, 
+    borderColor: '#01579b',
+    marginBottom: 10,
+    alignItems:'center', 
+  },
+});
+
+
+
+export default  Dashboard;
---
+++ pages/Goal_Summary.js
@@ -0,0 +1,109 @@
+import React, {Component} from 'react';
+import {Button, Text, View, StyleSheet, Image, TextInput, TouchableOpacity} from 'react-native';
+import {StackActions, NavigationActions} from 'react-navigation';
+import CheckBox from 'react-native-check-box'
+
+class Goal_Summary extends Component {
+  constructor(props){
+     super(props);
+     this.state = {
+        goToEventChecked: false,
+        tryNewClubChecked: false,
+        talkToSomeoneNewChecked: false,
+        hangOutWithFriendChecked: false,
+        customizedAction: '',
+     }
+  }
+   render() {
+    const { navigate } = this.props.navigation;
+    const firstName = this.props.navigation.getParam('name', 'GuitarBob97');
+    const actions = this.props.navigation.getParam('actions', 'NoActionYet');
+    const goalType = this.props.navigation.getParam('goalType', 'Rip');
+    this.state.summary = '';
+    this.state.count = 1;
+    for (let i = 0; i < 5; i++) {
+      if (actions[i] != '') {
+        this.state.summary+= "\n" + this.state.count.toString() + '. ' + actions[i];
+        this.state.count++;
+      } 
+    }
+    return(
+      <View style={styles.container}>
+        
+        <Text style={styles.titleText}>  Here is your Goal Summary! </Text> 
+        <View
+          style={styles.line}
+        /> 
+        <Text style={styles.actionText}>  To accomplish your new {goalType} goal, you will: {this.state.summary} </Text>
+        <View style={styles.button}>
+          <Button
+                title= "Submit"
+                color='#ffffff'
+                fontSize = '30'
+                onPress={() => {
+                  navigate('Dashboard', {name: firstName, goalType: goalType, newGoal: true, actions: actions});
+            }}
+          />       
+        </View>   
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#03a9f4',
+    flex: 1,
+  },
+  titleText: {
+    fontSize: 50,     
+    color: '#ffffff', 
+    marginLeft: 20
+  },
+  line: {
+    borderBottomColor: 'white',
+    borderBottomWidth: 5,
+    marginTop: 10,
+    marginBottom: 30
+  },
+  box: {
+    marginTop: 50,
+    padding: '50'
+  },
+  inputBox: {
+    width: 300,
+    height: 40,
+    fontSize: 18,
+    borderWidth: 2, 
+    borderRadius: 20,
+    borderColor: '#ffffff',
+    backgroundColor: 'rgba(255,255,255,0.5)',
+    marginBottom: 20,
+    marginLeft: 40,
+    marginTop: 20,
+    paddingHorizontal: 16,
+    color: '#ffffff'
+  },
+  button: {
+    width: 300,
+    height: 40,
+    backgroundColor: '#01579b',
+    borderRadius: 20,
+    borderWidth: 2, 
+    borderColor: '#01579b',
+    marginBottom: 10,
+    marginLeft: 40,
+    marginTop: 20
+  },
+  actionText: {
+    fontSize: 30, 
+    fontWeight: 'bold',    
+    color: '#ffffff',
+    alignItems:'center', 
+    paddingHorizontal: 30,
+  },
+
+  
+});
+
+export default Goal_Summary;
